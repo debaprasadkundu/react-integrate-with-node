7306d77439b34cc8d2252e00fbbe53f280ac616c
debaprasadkundu <83219536+debaprasadkundu@users.noreply.github.com>
2025-03-24 14:39:17 +0530

    file changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/data")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {});
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
